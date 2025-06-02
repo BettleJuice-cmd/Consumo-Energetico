cf1de60b9ab50ead7fa608a82f65b84fa5534a7f
BettleJuice-cmd <asdiazm@uamv.edu.ni>
2025-06-01 23:25:44 -0600

    Act

--- Registro_Asis.py
+++ Registro_Asis.py
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-"""
-Autor: Andy, D.
-Fecha: 01/06/2025
-Version: 1.0
-Descripcion: Desarrolle un programa que registre la asistencia de estudiantes en tres secciones distintas de
-la UAM, durante cinco días de clases. Cada día se tomará asistencia a seis estudiantes por
-sección. El programa deberá contabilizar y mostrar el total de asistencias registradas por
-sección, así como el total general de la semana. 
-"""
 
-dias_Semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]
-secciones = ["Sección A", "Sección B", "Sección C"]
-
-asistencia=[]
-
-
-for i in range(5):
-    print("Día:", dias_Semana[i])
-    for j in range(3):
-        print("Asistencia de la seccion: ", secciones[j])
-        seccion_Asistencia=[]
-        for k in range[6]:
-            Presente = input("El estudiante", k+1, "esta presente? (S/N): ")
-            if Presente == "S":
-                seccion_Asistencia.append("Presente")
-                print("El estudiante", k+1, "esta presente")
-            else:
-                print("El estudiante", k+1, "esta ausente")
-        print("Asistencia de la sección", secciones[j], "del día", dias_Semana[i], "registrada.")
-    secciones.append()
-    dias_Semana.append()
-
-        
-            
-        
-=======
 import os
 
 
@@ -97,4 +61,4 @@ for i in range(len(secciones)):
     print(f"{secciones[i]}: {totales_por_seccion[i]} asistencias TOTALES de la semana")
 
 print(f"Total general de asistencias en la semana: {total_general}")
->>>>>>> 512b855d655be0d593e85aa972024693e3685382
+ 
